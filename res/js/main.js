acc55e2e0d93cd5efaa18dee315ed77c87ac782f
nepperson89 <nicholas.epperson@smail.rasmussen.edu>
2024-08-18 19:20:32 -0500

    Update main.js

--- res/js/main.js
+++ res/js/main.js
@@ -1,1 +1,20 @@
-document.write('JavaSript saids "Hello!"');
+// Input for the user's first name
+let Fname = prompt("What is your first name?");
+
+// Welcome message for the user
+alert(`Welcome, ${Fname}!`);
+
+// Stored value for Pi
+const piValue = 3.1415926;
+
+// Input for the user's favorite number
+let myFavNum = prompt("What is your favorite number?");
+
+// Convert the user input to a number type
+myFavNum = parseFloat(myFavNum);
+
+// Calculate the area of a circle with the user’s favorite number
+let myArea = piValue * (myFavNum * myFavNum);
+
+// Display the contents of each variable in a message
+document.write(`Hello ${Fname}, you entered ${myFavNum} as your favorite number. If that was the radius of a circle, the circle's area would be ${myArea}.`);
